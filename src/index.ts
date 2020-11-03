9073a88f7fc85ba26faa5bc2a51429cef9f02105
James Carr <james@jamescarr.dev>
2020-11-04 08:33:47 +1000

    chore(deps): Bump outdated packages

--- src/index.ts
+++ src/index.ts
@@ -1,28 +1,30 @@
 import { startGroup, endGroup } from '@actions/core';
 import { issueCommand } from '@actions/core/lib/command';
-import { CLIEngine } from 'eslint';
+import { ESLint } from 'eslint';
 
-const GitHubActionsReporter: CLIEngine.Formatter = (results) => {
-  startGroup('Lint Annotations');
+const GitHubActionsReporter: ESLint.Formatter = {
+  format: (results) => {
+    startGroup('Lint Annotations');
 
-  const errors = results.flatMap((result) =>
-    result.messages.map((message) => ({
-      message: message.message,
-      properties: {
-        file: result.filePath,
-        line: message.line.toString(),
-        col: message.column.toString(),
-      },
-    }))
-  );
+    const errors = results.flatMap((result) =>
+      result.messages.map((message) => ({
+        message: message.message,
+        properties: {
+          file: result.filePath,
+          line: message.line.toString(),
+          col: message.column.toString(),
+        },
+      }))
+    );
 
-  for (const { message, properties } of errors) {
-    issueCommand('error', properties, message);
-  }
+    for (const { message, properties } of errors) {
+      issueCommand('error', properties, message);
+    }
 
-  endGroup();
+    endGroup();
 
-  return '';
+    return '';
+  },
 };
 
-export = GitHubActionsReporter;
+export = GitHubActionsReporter.format;
