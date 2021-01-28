a9a66e3c4c2d7bb063ee8352b53fa44cd06d0248
Jonas Lindenskov Nielsen <lindenskov.nielsen@gmail.com>
2021-01-28 13:41:20 +0100

    Adjust annotation output based on severity level

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,14 @@
 import { startGroup, endGroup } from '@actions/core';
 import { issueCommand } from '@actions/core/lib/command';
-import { ESLint } from 'eslint';
+import { ESLint, Linter } from 'eslint';
+
+// https://eslint.org/docs/4.0.0/user-guide/configuring#configuring-rules
+// https://github.com/actions/toolkit/blob/main/docs/commands.md#log-level
+const severityLabels: { [key in Linter.Severity]: string } = {
+  0: 'debug',
+  1: 'warning',
+  2: 'error',
+};
 
 const GitHubActionsReporter: ESLint.Formatter = {
   format: (results) => {
@@ -9,6 +17,7 @@ const GitHubActionsReporter: ESLint.Formatter = {
     const errors = results.flatMap((result) =>
       result.messages.map((message) => ({
         message: message.message,
+        severity: severityLabels[message.severity],
         properties: {
           file: result.filePath,
           line: message.line.toString(),
@@ -17,8 +26,8 @@ const GitHubActionsReporter: ESLint.Formatter = {
       }))
     );
 
-    for (const { message, properties } of errors) {
-      issueCommand('error', properties, message);
+    for (const { severity, properties, message } of errors) {
+      issueCommand(severity, properties, message);
     }
 
     endGroup();
